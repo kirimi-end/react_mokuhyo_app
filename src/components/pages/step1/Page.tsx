899f888c9808f6e6f2affa37cdaf3104b3de9dd8
ayumi kamada <dmrdmr58@gmail.com>
2022-10-06 22:11:41 +0900

    たまご追加

--- src/components/pages/step1/Page.tsx
+++ src/components/pages/step1/Page.tsx
@@ -3,11 +3,12 @@ import {
   Button,
   Container,
   Divider,
+  Grid,
   TextField,
   Typography,
 } from "@mui/material"
 import { Global, css } from "@emotion/react"
-import { relative } from "path"
+import Egg from "./Egg"
 
 const Page = () => {
   return (
@@ -23,25 +24,32 @@ const Page = () => {
       />
 
       <Box sx={{ mb: 2 }}>
-        <Typography
-          sx={{
-            backgroundColor: "white",
-            p: 2,
-            position: "relative",
-            "&::before": {
-              content: "''",
-              position: "absolute",
-              top: "calc(50% - 10px)",
-              left: "-10px",
-              width: "20px",
-              height: "20px",
-              backgroundColor: "white",
-              transform: "rotate(45deg)",
-            },
-          }}
-        >
-          飼い主の、あなたのお名前を 教えてください！
-        </Typography>
+        <Grid container>
+          <Grid item xs={3} sm={2}>
+            <Egg />
+          </Grid>
+          <Grid item xs={9} sm={10}>
+            <Typography
+              sx={{
+                backgroundColor: "white",
+                p: 2,
+                position: "relative",
+                "&::before": {
+                  content: "''",
+                  position: "absolute",
+                  top: "calc(50% - 10px)",
+                  left: "-10px",
+                  width: "20px",
+                  height: "20px",
+                  backgroundColor: "white",
+                  transform: "rotate(45deg)",
+                },
+              }}
+            >
+              飼い主の、あなたのお名前を 教えてください！
+            </Typography>
+          </Grid>
+        </Grid>
       </Box>
       <TextField
         fullWidth

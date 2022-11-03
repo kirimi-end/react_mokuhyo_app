739d2c6f342a01276d3c9b87ff3d39ad9f4f5e4b
marieono <104805318+marieono@users.noreply.github.com>
2022-11-03 21:59:03 +0900

    marginの調整

--- src/components/pages/step2/Page.tsx
+++ src/components/pages/step2/Page.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, TextField, Typography } from "@mui/material"
+import { Box, Container, Stack, TextField, Typography } from "@mui/material"
 import Balloon from "../../common/Balloon"
 import ConfirmButton from "../../common/ConfirmButton"
 import GlobalStyles from "~/components/common/GlobalStyles"
@@ -22,14 +22,16 @@ const Page = () => {
         />
       </Box>
 
-      <Box sx={{ textAlign: "center" }}>
-        <Typography>この子の名前は</Typography>
-        <TextField
-          sx={{
-            backgroundColor: "white",
-          }}
-        />
-        <Typography>だよ!</Typography>
+      <Box sx={{ textAlign: "center", mt: 2 }}>
+        <Stack spacing={2}>
+          <Typography>この子の名前は</Typography>
+          <TextField
+            sx={{
+              backgroundColor: "white",
+            }}
+          />
+          <Typography>だよ!</Typography>
+        </Stack>
       </Box>
 
       <Box sx={{ marginTop: "200px" }}>

e664e446b71b07062a0066b85af5c3132608a9b9
ayumi kamada <dmrdmr58@gmail.com>
2022-11-10 21:33:39 +0900

    吹き出し文字編集

---
+++ src/components/pages/explain/Page.tsx
@@ -0,0 +1,44 @@
+import { Box, Container, Stack, TextField, Typography } from "@mui/material"
+import Balloon from "../../common/Balloon"
+import ConfirmButton from "../../common/ConfirmButton"
+import GlobalStyles from "~/components/common/GlobalStyles"
+import Egg from "~/components/common/Egg"
+
+const Page = () => {
+  return (
+    <Container>
+      <Box sx={{ marginTop: "100px" }} />
+      <GlobalStyles />
+      <Box sx={{ mb: 2 }}>
+        <Balloon>タスクは色によって違いがあるよ</Balloon>
+      </Box>
+
+      <Box sx={{ textAlign: "center" }}>
+        <Egg
+          css={{
+            width: "118px",
+            height: "auto",
+          }}
+        />
+      </Box>
+
+      <Box sx={{ textAlign: "center", mt: 2 }}>
+        <Stack spacing={2}>
+          <Typography>この子の名前は</Typography>
+          <TextField
+            sx={{
+              backgroundColor: "white",
+            }}
+          />
+          <Typography>だよ!</Typography>
+        </Stack>
+      </Box>
+
+      <Box sx={{ marginTop: "200px" }}>
+        <ConfirmButton />
+      </Box>
+    </Container>
+  )
+}
+
+export default Page

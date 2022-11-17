edec3b038b41ea5dcfaef9b1e9b6374c49c89a70
marimonon <mappi-@hotmail.co.jp>
2022-11-17 21:43:23 +0900

    Dragonの顔を入れる。

--- src/components/pages/explain/Dragon.tsx
+++ src/components/pages/explain/Dragon.tsx
@@ -1,8 +1,17 @@
 import { Box, Typography } from "@mui/material"
+import DragonFace from "~/components/common/DragonFace"
 
 const Dragon = () => {
   return (
     <Box sx={{ backgroundColor: "white", p: 1 }}>
+      <Box sx={{ textAlign: "center" }}>
+        <DragonFace
+          css={{
+            width: "118px",
+            height: "auto",
+          }}
+        />
+      </Box>
       <Typography>習慣の竜</Typography>
       <Typography>
         毎日、毎週何曜日など、 決められた時に発生する竜 筋トレ、掃除、勉強など

69f40733c66886cc3d1402fb4d9c69fbdaf90394
marimonon <mappi-@hotmail.co.jp>
2023-01-01 19:25:21 +0900

    Timer背景の上に、ドラゴンちゃんとたまごちゃんを乗せた

--- src/components/pages/timer/Illust.tsx
+++ src/components/pages/timer/Illust.tsx
@@ -1,20 +1,39 @@
 // import { Box, Typography } from "@mui/material"
+import { Box, Container } from "@mui/system"
 import DragonTimer from "~/components/common/DragonTimer"
 import EggTimer from "~/components/common/EggTimer"
 import TimerBack from "~/components/common/TimerBack"
 
 const Illust = () => {
   return (
-    <>
+    <Box
+      css={{
+        position: "relative",
+      }}
+    >
       <TimerBack
         css={{
           width: "100%",
           height: "auto",
         }}
       />
-      <EggTimer />
-      <DragonTimer />
-    </>
+      <Container>
+        <Box
+          css={{
+            position: "absolute",
+            bottom: "30px",
+            left: "calc(50% - 140px)",
+            width: "280px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <DragonTimer />
+          <EggTimer />
+        </Box>
+      </Container>
+    </Box>
   )
 }
 

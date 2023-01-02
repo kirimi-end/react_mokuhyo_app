6ec0f4c5120b006e9017e3ecd10a747366f4290b
marimonon <mappi-@hotmail.co.jp>
2023-01-02 09:29:27 +0900

    タスク（仮：筋トレしてめっちゃ汗を流す）追加

--- src/components/pages/timer/Page.tsx
+++ src/components/pages/timer/Page.tsx
@@ -4,11 +4,15 @@ import Illust from "./Illust"
 import TimerCounter from "./TimerCounter"
 import TimerStart from "./TimerStart"
 import TimerTargetTime from "./TimerTargetTime"
+import TimerTask from "./TimerTask"
 
 const Page = () => {
   return (
-    <div css={{ paddingTop: "40px", backgroundColor: "#D1E1FA" }}>
+    <div
+      css={{ paddingTop: "40px", backgroundColor: "#D1E1FA", height: "100vh" }}
+    >
       <Container>
+        <TimerTask />
         <TimerStart />
         <TimerTargetTime />
         <TimerCounter />
---
+++ src/components/pages/timer/TimerTask.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/system"
+
+const TimerTask = () => {
+  return (
+    <Box
+      sx={{
+        fontWeight: "bold",
+        fontSize: "24px",
+        textAlign: "center",
+        marginTop: "20px",
+      }}
+    >
+      筋トレしてめっちゃ汗を流す。代謝アップ
+    </Box>
+  )
+}
+
+export default TimerTask
